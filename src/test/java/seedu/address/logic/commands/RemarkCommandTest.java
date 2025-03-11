df9b62a7420883b5d49da8bdeccb02d870e2e671
raw-asparagus <66353964+raw-asparagus@users.noreply.github.com>
2025-03-11 17:23:49 +0800

    Add tests and parsing for RemarkCommand

---
+++ src/test/java/seedu/address/logic/commands/RemarkCommandTest.java
@@ -0,0 +1,30 @@
+package seedu.address.logic.commands;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
+
+/**
+ * Contains integration tests (interaction with the Model) and unit tests for RemarkCommand.
+ */
+public class RemarkCommandTest {
+
+    private Model model;
+    private Model expectedModel;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
+        expectedModel = new ModelManager(model.getAddressBook(), new UserPrefs());
+    }
+
+    @Test
+    public void execute() {
+        assertCommandFailure(new RemarkCommand(), model, "Hello from remark");
+    }
+}
